f92b81776f97954931b9403c9f4fe9a51b8d38be
Daniel-Azil <125911921+Daniel-Azil@users.noreply.github.com>
2023-08-09 05:09:39 +0000

    Validated pycodestyle format

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,8 @@ number = random.randint(-10000, 10000)
 if number >= 0:
     if abs(number) % 10 == 0:
         last_number_var1 = (number) % 10
-        print("Last digit of {:d} is {:d} and is 0".format(number, last_number_var1))
+        print("Last digit of {:d} is {:d} and is "
+               "0".format(number, last_number_var1))
     elif abs(number) % 10 > 5:
         last_number_var2 = (number) % 10
         print("Last digit of {:d} is {:d} and is greater "
@@ -12,12 +13,13 @@ if number >= 0:
     elif abs(number) % 10 < 6:
         last_number_var3 = (number) % 10
         print("Last digit of {:d} is {:d} and is less than 6 "
-        "and not 0".format(number, last_number_var3))
+               "and not 0".format(number, last_number_var3))
 if number < 0:
     if number % -10 == 0:
         last_number_var4 = number % -10
-        print("Last digit of {:d} is {:d} and is 0".format(number, last_number_var4))
+        print("Last digit of {:d} is {:d} and "
+               "is 0".format(number, last_number_var4))
     elif number % -10 < 0:
         last_number_var5 = number % -10
         print("Last digit of {:d} is {:d} and is less than "
-              "6 and not 0".format(number, last_number_var5))
+               "6 and not 0".format(number, last_number_var5))

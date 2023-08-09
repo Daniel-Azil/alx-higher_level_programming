0c49aed0af9e5fe233c55c05bb802acf97ff5aa0
Daniel-Azil <125911921+Daniel-Azil@users.noreply.github.com>
2023-08-09 05:23:37 +0000

    Validated pycodestyle format

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,9 +3,9 @@ def print_last_digit(number):
     if number >= 0:
         variable = number % 10
         print("{:d}".format(variable), end='')
-        return(variable)
+        return (variable)
     else:
         variable = number % -10
         variable *= -1
         print("{:d}".format(variable), end='')
-        return(variable)
+        return (variable)

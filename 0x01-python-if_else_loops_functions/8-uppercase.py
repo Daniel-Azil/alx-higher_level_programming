4db59bef8028723f77e926f835c164c109688ec1
Daniel-Azil <125911921+Daniel-Azil@users.noreply.github.com>
2023-08-09 05:21:35 +0000

    validated pycodestyle format

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     for alphabet_character in range(len(str)):
-        if ord(str[alphabet_character]) >= 97 and ord(str[alphabet_character]) <= 122:
+        if ord(str[alphabet_character])
+           >= 97 and ord(str[alphabet_character]) <= 122:
             num = 32
         else:
             num = 0

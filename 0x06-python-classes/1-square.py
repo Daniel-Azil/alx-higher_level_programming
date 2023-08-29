c1c8dbf0d862a00f98dae4097ebb6e068165d1bc
Daniel Azil <danielazil.fse@gmail.com>
2023-08-29 23:33:38 +0000

    created class that has private Attributes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Initialization of Class """
+
+class Square:
+    """ Defining a Method """
+    def __init__(object, size):
+        """ Method  Attribute"""
+        object.__size = size

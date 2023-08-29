25f381f47ca8c02014ebd703903335d25d38b88f
Daniel-Azil <125911921+Daniel-Azil@users.noreply.github.com>
2023-08-29 23:52:35 +0000

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-""" Initialization of Class
-
-    Contains the __init__ method
-
-    Initialises Private Attribute """
+""" Initializes Class & Private Attribute """
 
 class Square:
     """ Defining a Method """
 
     def __init__(self, size):
         """ Method  Attribute
-            
-            Args:
-                 size (int): Private Attribute of the class.
+        Args:
+            size (int): Private Attribute of the class.
         """
         self.__size = size

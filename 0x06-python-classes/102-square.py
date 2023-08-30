f7d4e3edd02edd900832a0ceb4bdeb89000892a3
Daniel-Azil <125911921+Daniel-Azil@users.noreply.github.com>
2023-08-30 06:47:49 +0000

    Update 102-square.py

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -5,7 +5,7 @@
 class Square:
     """ initialized class named sqaure"""
 
-    def __init__(self, size=0)::
+    def __init__(self, size=0):
         """ method instantiation """
         self.size = size
 

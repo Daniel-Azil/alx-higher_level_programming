530e669951d9ac81b004df20d45abfb8c42e7338
Daniel-Azil <125911921+Daniel-Azil@users.noreply.github.com>
2023-08-30 00:32:34 +0000

    corrected value error condition

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,7 +7,7 @@ class Square:
     def __init__(object, size):
         if type(size) is not int:
             raise TypeError("size must be an integer")
-        elif size <= 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         else:
             object.__size=size

3f15f70e745560e5ad337e0d27d2d431d70c19d9
Daniel-Azil <125911921+Daniel-Azil@users.noreply.github.com>
2023-08-30 06:17:55 +0000

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-""" Initilization of a class """
+""" Initilization of a class."""
 
 
 class Square:
-    """ Defined a class named Square """
+    """ Defined a class named Square."""
 
     def __init__(self, size):
-        """ Made an instantiation"""
+        """ Made an instantiation."""
         self.size = size
 
     @property
     def size(self, size):
-        """ initialised getter """
+    """Getter and setter of size for square."""
         return (self.__size)
 
     @size.setter
     def size(self, value):
-        """ initialised setter """
+        """ initialised setter."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -24,5 +24,5 @@ class Square:
         self.__size = value
 
     def area(self):
-        """ initialised method named area """
+        """ initialised method named area."""
         return (self.__size * self.__size)

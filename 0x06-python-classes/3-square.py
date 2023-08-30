29a0cfb8e848a2fcbf2c613bae59d450f9ff5775
Daniel Azil <danielazil.fse@gmail.com>
2023-08-30 03:41:19 +0000

    made corrections to pycoding style

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -6,10 +6,12 @@ class Square(self, size):
     """ Defining a class named Square """
 
     def __init__(self, size):
-        """ 
+        """
+
         Initialising instance private attribute
         defining conditions of type and value errors
         """
+
         if type(size) is not int:
             print("size must be an integer")
         elif size < 0:
@@ -18,4 +20,4 @@ class Square(self, size):
             self.__size = size
 
     def area(self):
-        return(self.__size * self.__size)
+        return (self.__size * self.__size)

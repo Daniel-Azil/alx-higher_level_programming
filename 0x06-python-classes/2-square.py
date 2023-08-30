6583ccae0d96d27210bcd6828006554cd77852ee
Daniel-Azil <125911921+Daniel-Azil@users.noreply.github.com>
2023-08-30 00:38:28 +0000

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,10 +4,10 @@
 
 class Square:
     """ Initializing main method """
-    def __init__(object, size):
+    def __init__(self, size):
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
-            object.__size = size
+            self.__size = size

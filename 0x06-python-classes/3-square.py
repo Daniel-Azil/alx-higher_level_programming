51b8d2ba1a9f32d9e37e9edbfc53be1b1301b6ed
Daniel-Azil <125911921+Daniel-Azil@users.noreply.github.com>
2023-08-30 06:11:54 +0000

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" class Square that defines a square"""
+""" defined a class name square"""
 
 
 class Square:
-    """ class Square that defines a square"""
+    """ defined a class named a square"""
 
     def __init__(self, size=0):
         """initialize size of a square

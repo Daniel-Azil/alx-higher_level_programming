b6f21f60a1022c8955b140ce3599c9b5385e0e6d
Daniel Azil <danielazil.fse@gmail.com>
2023-09-04 22:24:57 +0000

    created a class with no functionalities

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" 
+    A class that uses the pass statement to
+    prevent errors from the intepreter since there
+    are no functionalities in the class
+
+"""
+
+
+class Rectangle:
+    """ 
+         args: no arguments passed to
+               the class
+    """
+    pass

505989f023aef90a1c597fbd18551dfd0c2e843c
Daniel Azil <danielazil.fse@gmail.com>
2023-09-04 23:42:51 +0000

    added __repr__ functionality to class

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+""" Initialsing class with constructor, getter, setter, __str__
+    and __repr__ functionality
+"""
+
+
+class Rectangle:
+    """ initializing constructor with instance attributes"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ method with getter functionality
+        """
+        return self.__width
+
+    @property
+    def height(self):
+        """ method with getter functionality
+        """
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """ method with setter functionality
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """ method with setter functionality
+        """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """method that returns area of the class rectangle """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """ method that returns the perimiters of the class"""
+        if self.__width is 0 or self.__height is 0:
+            return 0
+        return self.__width * 2 + self.__height * 2
+
+    def __str__(self):
+        """ method with __str__ functionaltiy to return #
+        """
+        if self.__width is 0 or self.__height is 0:
+            return ""
+        return ("\n".join(["".join(["#" for i in range(self.__width)])
+                for j in range(self.__height)]))
+
+    def __repr__(self):
+        """ method that return given strings
+        """
+        return "Rectangle({}, {})".format(self.__width, self.__height)

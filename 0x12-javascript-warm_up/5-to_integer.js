5bd5c923566243f8069d280cae97bab7142081f9
Daniel Azil <danielazil.fse@gmail.com>
2023-11-29 16:40:32 +0000

    changed string double quotes to single quote

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -7,7 +7,7 @@ const cmd_arg = Math.floor(Number(process.argv[2]));
 // Check if the converted number is NaN (Not a Number)
 if (isNaN(cmd_arg)){
     // Print a message if the argument is not a valid number
-    console.log("Not a number")
+    console.log('Not a number')
 }
 else{
     // Print the converted number if it's a valid number
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 //script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
-const str_var = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const str_var = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let start= 0; start < str_var.length; start++)
 {
     console.log(str_var[start]);

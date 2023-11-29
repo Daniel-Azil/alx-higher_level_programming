8b6b97eb6caf9151483eeead9af43bb7c2fcf391
Daniel Azil <danielazil.fse@gmail.com>
2023-11-29 15:10:16 +0000

    updated comments

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
-// The above line is a shebang, specifying that this script should be run using Node.js
-
+//script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
 const str_var = ["C is fun", "Python is cool", "JavaScript is amazing"];
-// The above line is a variable declaration, declaring a string array named str_var
 
 for (const ele of str_var)
-// The above line is a for loop, iterating over the elements of str_var
 {
     console.log(ele);
-// prints lists of strings iterated
 }

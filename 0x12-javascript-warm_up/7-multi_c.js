d3ad13edd8aa87eb4e144f8e1786e927554a9700
Daniel Azil <danielazil.fse@gmail.com>
2023-11-29 16:36:08 +0000

    changed string double quotes to single quote

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,9 +3,9 @@
 
 const arg = Number(process.argv[2]);
 if (isNaN(arg)) {
-    console.log("Missing number of occurrences");
+    console.log('Missing number of occurrences');
 }
 else {
     for (let itr = 0; itr < arg; itr++) {
-        console.log("C is fun"); }
+        console.log('C is fun'); }
 }

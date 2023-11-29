18428c750e7da870f3970345f3c488868c2fba79
Daniel Azil <danielazil.fse@gmail.com>
2023-11-29 17:12:51 +0000

    changed string double quotes to single quote

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 //script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
 const str_var = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let start= 0; start < str_var.length; start++)
-{
+
+for (let start= 0; start < str_var.length; start++) {
     console.log(str_var[start]);
 }
--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const arg = Number(process.argv[2]);
+const arg = Math.floor(Number(process.argv[2]));
 if (isNaN(arg)) {
-    console.log('Missing number of occurrences');
-}
-else {
-    for (let itr = 0; itr < arg; itr++) {
-        console.log('C is fun'); }
+  console.log('Missing number of occurrences');
+} else {
+  for (let itr = 0; itr < arg; itr++) {
+    console.log('C is fun');
+  }
 }

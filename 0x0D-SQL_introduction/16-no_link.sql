7f61bd25a39034a4131fb96d1e689db2ee57dd17
Daniel Azil <danielazil.fse@gmail.com>
2023-12-08 18:52:55 +0000

    A script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,7 @@
+-- A script that lists all records of the table second_table
+-- of the database hbtn_0c_0 in your MySQL server.
+
+SELECT `score`, `name`
+FROM `school_table`
+WHERE `name` != ""
+ORDER BY `score` DESC;

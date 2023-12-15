d02287c3d12fdcb52c535a4243e19f32d23660dd
Daniel Azil <danielazil.fse@gmail.com>
2023-12-15 22:53:46 +0000

    Updated script to pycodestyle format

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,8 @@ import sys
 
 if __name__ == "__main__":
     dataBase_connect = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                                       passwd=sys.argv[2], db=sys.argv[3], port=3306)
+                                       passwd=sys.argv[2], db=sys.argv[3],
+                                       port=3306)
     cursor = dataBase_connect.cursor()
     cursor.execute("SELECT * FROM states")
     records = cursor.fetchall()

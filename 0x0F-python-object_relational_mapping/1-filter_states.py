23e2bcfc35519d44bb3a73add7fb6332af2aafb6
Daniel Azil <danielazil.fse@gmail.com>
2023-12-15 23:56:22 +0000

    Corrected query and adhere to pycodestyle

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 
-""" A script that lists all states with a name starting 
+""" A script that lists all states with a name starting
     with N (upper N) from the database hbtn_0e_0_usa
 """
 
 import MySQLdb
 import sys
 
-database_connect = MySQLdb.connect(host= "localhost", user= sys.argv[1], 
-                                   passwd= sys.argv[2], db= sys.argv[3],
-                                   port= 3306)
+database_connect = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                                   passwd=sys.argv[2], db=sys.argv[3],
+                                   port=3306)
 cursor = database_connect.cursor()
-cursor.execute("""SELECT * FROM states 
-               WHERE name LIKE BINARY 'N%' ORDER BY state.id""")
+cursor.execute("""SELECT * FROM states
+               WHERE name LIKE BINARY 'N%' ORDER BY states.id""")
 records = cursor.fetchall()
 for values in records:
     print(values)

31ea6c82983f85c88e4ec206b0188ee53c4a2cc7
Daniel Azil <danielazil.fse@gmail.com>
2023-12-16 00:21:10 +0000

    A script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+""" A script that takes in an argument and displays all values in the
+    states table of hbtn_0e_0_usa where name matches the argument.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    database_connect = MySQLdb(host="localhost", user=sys.argv[1],
+                               passwd=sys.argv[2], db=sys.argv[3],
+                               port="3306")
+    cursor = database_connect.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                   .format(sys.argv[4]))
+    records = cursor.fetchall()
+    for values in records:
+        print(values)
+    cursor.close()
+    database_connect.close()

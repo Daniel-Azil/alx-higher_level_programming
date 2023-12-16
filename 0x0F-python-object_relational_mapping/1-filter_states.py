9f0d2b1b670d0c77ded17cc02457b4602ca173dc
Daniel Azil <danielazil.fse@gmail.com>
2023-12-16 00:07:29 +0000

    added space around operator '=='

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,7 +7,7 @@
 import MySQLdb
 import sys
 
-if __name__=="__main__":
+if __name__ == "__main__":
     database_connect = MySQLdb.connect(host="localhost", user=sys.argv[1],
                                        passwd=sys.argv[2], db=sys.argv[3],
                                        port=3306)

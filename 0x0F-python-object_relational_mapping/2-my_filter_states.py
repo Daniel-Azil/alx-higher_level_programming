c8137b58e3bca683616863080d07cf9245023483
Daniel Azil <danielazil.fse@gmail.com>
2023-12-16 00:26:58 +0000

    Corrected port datatype

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,7 +10,7 @@ import sys
 if __name__ == "__main__":
     database_connect = MySQLdb.connect(host="localhost", user=sys.argv[1],
                                passwd=sys.argv[2], db=sys.argv[3],
-                               port="3306")
+                               port=3306)
     cursor = database_connect.cursor()
     cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
                    .format(sys.argv[4]))

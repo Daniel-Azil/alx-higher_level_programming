f1cc62dc1baa67a66541964cab3d1276a4c32ff4
Daniel Azil <danielazil.fse@gmail.com>
2023-12-17 14:09:11 +0000

    made some corrections

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
+"""
+return all state objects from database via python
+parameters given to script: username, password, database
+"""
 
-""" script that lists all State objects from the database hbtn_0e_6_usa"""
-
-import sqlalchemy
-import sys
+from sys import argv
 from model_state import Base, State
-import sqlalchemy.orm
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
 
-    user = sys.argv[1]
-    passwd = sys.argv[2]
-    db = sys.argv[3]
-    engine = sqlalchemy.create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                                      format(user, passwd, db),
-                                      pool_pre_ping=True)
-    Session = sqlalchemy.orm.sessionmaker(bind=engine)
+    # make engine for database
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(user, passwd, db), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
     session = Session()
 
-    for records in session.query(State).order_by(State.id):
-        print("{:d}: {:s}".format(records.id, records.name))
+    # query python instances in database
+    for instance in session.query(State).order_by(State.id):
+        print("{:d}: {:s}".format(instance.id, instance.name))
+
     session.close()

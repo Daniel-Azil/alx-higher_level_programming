18ecce7eab6ace153b55e3922b504b031c503777
Daniel Azil <danielazil.fse@gmail.com>
2023-12-17 23:47:13 +0000

    corrected id string

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -14,6 +14,6 @@ class City(Base):
     """
     __tablename__ = "cities"
 
-    id = Column(Integer, nullable=False, primary_key=True)
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey=(State.id), nullable=False)
+    state_id = Column(Integer, ForeignKey=("states.id"), nullable=False)
